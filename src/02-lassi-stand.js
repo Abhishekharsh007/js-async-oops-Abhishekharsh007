07e4ec2e637ea4c82721c3ba8fc5be67d0feba94
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-03-23 23:29:43 +0530

    Implement LassiStand class with methods for adding flavors, taking orders, completing orders, calculating revenue, and retrieving the menu

--- src/02-lassi-stand.js
+++ src/02-lassi-stand.js
@@ -73,15 +73,79 @@
  */
 export function LassiStand(name, city) {
   // Your code here
+  this.name = name;
+  this.city = city;
+  this.menu = [];
+  this.orders = [];
+  this._nextOrderId = 1;
 }
 
 // Add prototype methods here:
-// LassiStand.prototype.addFlavor = function(flavor, price) { ... }
-// LassiStand.prototype.takeOrder = function(customerName, flavor, quantity) { ... }
-// LassiStand.prototype.completeOrder = function(orderId) { ... }
-// LassiStand.prototype.getRevenue = function() { ... }
-// LassiStand.prototype.getMenu = function() { ... }
+LassiStand.prototype.addFlavor = function (flavor, price) {  
+  if (price <= 0) {
+    return -1;
+  } 
+
+  const found = this.menu.find((item) => item.flavor === flavor);
+
+  if (found === undefined) {
+    this.menu.push({ flavor, price });
+    return this.menu.length;
+  }
+
+  return -1;
+}
+
+LassiStand.prototype.takeOrder = function (customerName, flavor, quantity) {  
+  if (quantity <= 0) {
+    return -1;
+  }
+
+  const found = this.menu.find((item) => item.flavor === flavor);
+  if (found === undefined) {
+    return -1;
+  }
+
+  let totalPrice = found.price * quantity;
+  let order = {
+    id: this._nextOrderId++,
+    customer: customerName,
+    flavor,
+    quantity,
+    total: totalPrice,
+    status: "pending",
+  };
+
+  this.orders.push(order);
+  
+  return order.id;
+}
+
+LassiStand.prototype.completeOrder = function (orderId) { 
+  const orderFound = this.orders.find((item) => item.id === orderId);
+  if (orderFound === undefined || orderFound.status === "completed") {
+    return false;
+  }
+  orderFound.status = "completed";
+  return true;
+}
+
+LassiStand.prototype.getRevenue = function () {  
+  let getRev = 0;
+  for (let item of this.orders) {
+    if (item.status === "completed") {
+      getRev += item.total;
+    }
+  }
+  return getRev;
+}
+
+LassiStand.prototype.getMenu = function () {  
+  let arr = [...this.menu];
+  return arr;
+}
 
 export function isLassiStand(obj) {
   // Your code here
+  return obj instanceof LassiStand;
 }
